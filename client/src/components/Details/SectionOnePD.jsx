7e0152d368a3219ce811583f2a00d9d4708f96d7
Horus Sinhue Camacho <62359674+horuscamacho@users.noreply.github.com>
2022-10-20 07:00:33 -0500

    Lista la de detalles

--- client/src/components/Details/SectionOnePD.jsx
+++ client/src/components/Details/SectionOnePD.jsx
@@ -1,43 +1,25 @@
 import React from "react";
-import imgDum from "../../assets/post-1.png";
+import { useParams } from "react-router-dom";
+import { proyectos } from "../data/proyects_certificates";
+
 
 const SectionOnePD = () => {
+  const { id } = useParams()
+  const allProjects = proyectos[1]
+  const project = allProjects.filter((element) => element.id === parseInt(id))
+  console.log(project)
   return (
     <>
       <section className="blogpost-img-container">
-        <img src={imgDum} alt="" />
+        <img src={project[0].link} alt="" />
       </section>
       <section>
         <div>
           <div className="blogpost-title-container">
-            <h3>Titulo</h3>
+            <h3>{project[0].proyecto}</h3>
           </div>
           <article className="blogpost-detail-container">
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Blanditiis dignissimos iure rem, ipsa molestiae obcaecati adipisci
-              nam dicta unde dolor voluptatum ipsum aperiam, voluptate est
-              recusandae aliquid exercitationem repellendus. Velit?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-              officia totam velit, odio fuga maxime esse eius quod minus cum
-              doloribus minima, excepturi animi a saepe? Quidem sed voluptate
-              fugit?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-              molestias cupiditate quidem repellat alias, ab inventore quam
-              adipisci, dolore mollitia id omnis quas consequatur. Voluptatibus
-              in eius aliquid natus architecto? Lorem ipsum dolor, sit amet
-              consectetur adipisicing elit. Est repellendus voluptatum,
-              molestiae impedit eveniet quia obcaecati consequuntur laborum.
-              Doloremque nemo nulla repudiandae minima minus odio corporis ipsam
-              fugit optio deserunt! Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Odio, reiciendis illo libero placeat optio
-              voluptate fugiat magnam quam quod aut voluptatem totam iusto quo
-              nobis minima necessitatibus similique! Architecto, officia.
-            </p>
+            <p>{project[0].details}</p>
           </article>
         </div>
       </section>
