8899b6894de8e23473988999cec4648a3f86fd92
Horus Sinhue Camacho <62359674+horuscamacho@users.noreply.github.com>
2022-10-20 06:35:18 -0500

    Terminando de actualizar los datos de proyectos y cursos

--- client/src/components/data/proyects_certificates.js
+++ client/src/components/data/proyects_certificates.js
@@ -2,32 +2,47 @@ export const proyectos = [
   ["Proyectos"],
   [
     {
-      id: 10,
+      id: 1,
       proyecto: "Pokedex Henry",
       descripcion:
         "Construir una App utilizando React, Redux, Node y Sequelize, crear filtros y ordenamientos para los elementos, crear nuevos",
       link: "https://i.postimg.cc/QtR6w580/Disen-o-sin-ti-tulo.png",
       github: "https://github.com/horuscamacho/PI-pokemons",
       deploy: "",
+      details:
+        "Al finalizar la cursada del Bootcamp en Henry, para poner en práctica lo aprendido, desarrollamos una aplicación en la cual hacemos peticiones que apuntan hacia APIS de terceros. Los principales retos del proyecto individual consisten en crear una single page aplication, la aplicación debe tener una base de datos donde podremos crear nuevos elementos; además debemos poder filtrar y ordenar elementos.",
     },
     {
-      id: 11,
+      id: 2,
       proyecto: "TICKETSHOP Henry",
       descripcion:
         "Construir una App con autenticación propia y de terceros, borrado lógico, notificiaciones e-mail, filtros combinados y más...",
       link: "https://i.postimg.cc/MpTFczF0/Disen-o-sin-ti-tulo-1.png",
-      github: "",
+      github: "https://github.com/horuscamacho/TICKET-HENRY",
       deploy: "https://tickethenry.vercel.app/",
+      details:
+        "TICKETSHOP es una sigle web page aplication, fue desarrollada con tecnologías como React, Redux, Tailwind CSS entre otras. TICKET Henry es un e-commerce diseñado para vender/comprar boletos para los mejores eventos tanto deportivos, musicales y mucho más. Este proyecto fue desarrollado por mis compañeros de equipo 'Luis', 'Joaco', 'Lucho', 'Cristian' y 'Jona' con quienes trabajé para desarrollar esta plataforma desde cero. La principales funcionalidades de la plataforma son: Autenticación de terceros y propia, deployment, notificaciones(vía e-mail), filtros combinados, panel de administrador, borrado lógico, almacenamiento de imágenes en la nube, sistema de reviews y pasarela de pagos entre otros.",
     },
     {
-      id: 12,
+      id: 3,
       proyecto: "Responsive Design Product",
       descripcion:
         "Construir y maquetar una página estática de forma responsiva para entender el funcionamiento de los breakpoints",
       link: "https://i.postimg.cc/CLXwLrvK/Disen-o-sin-ti-tulo-2.png",
-      github: "",
+      github: "https://github.com/horuscamacho/responsive-design-product",
       deploy: "",
+      details: "Una de las principales ventajas del Mobile First es su escalabilidad, es decir, se puede ir adaptando a las necesidades de los usuarios y crecer en función de las posibilidades de la empresa en cuanto a tiempo de respuesta de servicio o abastecimiento. Es por eso que a través del curso 'Responsive Design Product' en la plataforma de platzi, construí y maquete una página estática de forma responsiva para entender el funcionamiento de los breakpoints y además utilizamos como guía de maquetación elementos contenidos en la plataforma Figma."
     },
+    {
+      id: 4,
+      proyecto: "Portafolio Personal",
+      descripcion:
+        "Mi portafolio fue desarrollado con tecnologías simples como React y Redux para el manejo de los estados de componentes, además utilice los conceptos aprendidos del Responsive Design para contruir mi maquetado de forma Mobile First",
+      link: "https://i.postimg.cc/L67XNX0R/portafolio.png",
+      github: "https://github.com/horuscamacho/project-portfolio-MF",
+      deploy: "",
+      details: "Para el desarrollo de mi portafolio, modularice mis componentes y utilice CSS puro principalmente con flexbox para lograr que mi portafolio sea responsivo, cree 3 breakpoints diferentes que servirán para el correcto renderizado dependiendo el tamaño de la pantalla. Además integré un formulario de contacto que se conectara al back para poder enviarme una notificación cuando quieran ponerse en contacto conmigo. La intención es poder escalar el proyecto para poder generar una base de datos y poder generar de forma automática desde un formulario la creación de nuevos proyectos y cursos que serán renderizados en las secciones correspondientes de mi proyecto."
+    }
   ],
 ];
 
@@ -36,24 +51,39 @@ export const cursos = [
   [
     {
       id: 1,
+      curso: "Full Stack Web Developer",
+      descripcion:
+        "Bootcamp de Henry, entrenamiento intensivo en el que enseñan los conceptos básicos para comenzar a programar, hasta profundizar en detalle los conocimientos técnicos que son necesarios para convertirse en un desarrollador Full Stack. ",
+      link: "https://i.postimg.cc/FH620V2h/henry.png",
+    },
+    {
+      id: 2,
+      curso: "EF SET Certificate C1 Advanced",
+      descripcion:
+        "The EF Standard English Test (EF SET) is the world´s first completely free, online standardized English test for learners of all levels, from beginner to advanced. As part of EF Education First, we draw upon a 50-year legacy",
+      link: "https://i.postimg.cc/52SWTD9Y/Captura-de-Pantalla-2022-10-12-a-la-s-15-34-45.png",
+    },
+    {
+      id: 3,
+      curso: "Fundamentos de Typescript",
+      descripcion:
+        "Conocer los datos primitivos y especiales del lenguaje para programar aplicaciones web, agilizar su mantenimiento y evitar la mayoría de errores. Descubre los fundamentos de TypeScript para agregar tipado fuerte al código JS",
+      link: "https://i.postimg.cc/HxWZKS8G/typescript.png",
+    },
+    {
+      id: 4,
       curso: "Curso Práctico de Maquetación en CSS",
       descripcion:
         "Crear un espacio personal para reforzar conocimientos en maquetación de HTML5 semántico, maquetando el header, el main y el footer, creando estilos para el proyecto de forma estética",
       link: "https://i.postimg.cc/VkszpFt4/diploma-practico-css.png",
     },
     {
-      id: 2,
+      id: 5,
       curso: "Resp Design: Maquetación Mobile First",
       descripcion:
         "Aprender a crear prouectos que se adaptarán a las distintas vistas que utilicen los usuarios, se analizó la arquitectura de un proyecto partiendo de un wireframe, auditado, con lighthouse. ",
       link: "https://i.postimg.cc/k42DVPv2/diploma-mobile-first-1.png",
     },
-    {
-      id: 3,
-      curso: "EF SET Certificate C1 Advanced",
-      descripcion:
-        "The EF Standard English Test (EF SET) is the world´s first completely free, online standardized English test for learners of all levels, from beginner to advanced. As part of EF Education First, we draw upon a 50-year legacy",
-      link: "https://i.postimg.cc/52SWTD9Y/Captura-de-Pantalla-2022-10-12-a-la-s-15-34-45.png",
-    },
+    
   ],
 ];
