3acfba6f4506e9dbc2bb0faad4dad6e7a8091bc6
Horus Sinhue Camacho <62359674+horuscamacho@users.noreply.github.com>
2022-10-21 09:11:04 -0500

    Hay que ver si conviene cambiar el envío de correo desde el back o desde el front

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,9 @@
 const express = require('express') 
 const server = express()
+const cors = require("cors")
 
+server.use(cors())
+server.use(express.json())
 server.use("/", require('./router/routers'));
 
 module.exports = server;
--- api/src/controllers/email.js
+++ api/src/controllers/email.js
@@ -1,14 +1,46 @@
+const { createTransport } = require("nodemailer");
+
+const sendEmail = async (req, res, next) => {
+  let password = "jdwosmhgbyqjjzls";
+  let correo = "tickethenryinfo@gmail.com";
+
+  const {name, email, message } = req.body
+
+  const transporter = createTransport({
+    service: "gmail",
+    port: 587,
+    auth: {
+      user: correo,
+      pass: password,
+    },
+  });
+
+  const mailOptions = {
+    from: "TicketShop",
+    to: "camachohorus@gmail.com",
+    subject: "Correo desde el portafolio",
+    template: "email",
+    text: `Mensaje de ${name}, enviado desde el correo ${email} con el mensaje: 
+    ${message}`
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Correo enviado satisfactoriamente");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 const enElServidor = async (req, res, next) => {
-    try {
-        console.log("Ya entraste al servidor")
-    } catch (error) {
-        console.log("no lograste entrar al servidor")
-    }
-}
-
-
+  try {
+    console.log("Ya entraste al servidor");
+  } catch (error) {
+    console.log("no lograste entrar al servidor");
+  }
+};
 
 module.exports = {
-    enElServidor
-}
+  enElServidor,
+  sendEmail,
+};
